b2cfda6d17d0937226a47867f91d5a4b24ec5d3d
Charlie Coglianese <charles.coglianese@gmail.com>
2011-12-30 16:26:27 -0700

    Clean up junk.

--- views/view_404.php
+++
@@ -1,6 +0,0 @@
-<script type="text/javascript">head(function(){ mpq.track('404', { "Name": "404" }); });</script>
-<div class="jobs">
-    <div class="content">
-        <img src="images/404.png" alt="404" />
-    </div>
-</div>
